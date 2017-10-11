635881db063677194a1bc3a0eb21874f3640bec2
Naomi <yukimono6@gmail.com>
2017-10-11 13:10:00 +0100

    [types] update definition

--- types/nydus.d.ts
+++ types/nydus.d.ts
@@ -1,7 +1,9 @@
 type NYDUS_PROTOCOL = {
   VERSION: number;
   TYPE_INFO: Array<[string, any[]]>;
-  GAME_EVENT: { [key: string]: any[] }
+  GAME_EVENT: { [key: string]: any[] };
+  MESSAGE_EVENT: { [key: string]: any[] };
+  TRACKER_EVENT: { [key: string]: any[] };
   REPLAY: number[];
 };
 
