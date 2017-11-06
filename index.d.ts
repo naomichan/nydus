0fc4dbb25b9b6ced28997bef15456ef3b0ea02e8
Naomi <yukimono6@gmail.com>
2017-11-06 17:15:56 +0000

    [fix]

--- index.d.ts
+++ index.d.ts
@@ -5,4 +5,43 @@
 /// <reference path="./versioned.d.ts" />
 /// <reference path="./tool/heroprotoc.d.ts" />
 /// <reference path="./bin/nydusgen.d.ts" />
-/// <reference path="./nydus.d.ts" />
+import { MPQArchive } from "mpyqjs/mpyq";
+export declare enum GameType {
+    HERO = 0,
+    S2 = 1,
+    ANY = 4096,
+}
+export declare class Replay {
+    mpq: MPQArchive;
+    protocol: NYDUS_PROTOCOL | null;
+    attributes: NYDUS_ATTRIBUTES;
+    header: any;
+    parent: Parser;
+    constructor(file: string, protocol: NYDUS_PROTOCOL, attrib: NYDUS_ATTRIBUTES, parent: Parser);
+    loadProtocol(): Promise<boolean>;
+    ready(): boolean;
+    parseDetails(): any;
+    parseInitData(): any;
+    parseGameEvents(onEvent: Function): any;
+    parseMessageEvents(onEvent: Function): any;
+    parseTrackerEvents(onEvent: Function): any;
+    parseAttributeEvents(): any;
+    private varInt(decoder);
+    private decodeEventStream(decoder, onEvent, eventidTypeid, eventTypes, decodeUserId);
+}
+export declare class Parser {
+    protected gameType: GameType;
+    protected defDir: string;
+    protected LATEST_PROTO: NYDUS_PROTOCOL | null;
+    protected shouldDownloadProtocol: boolean;
+    constructor(decl: GameType | string, shouldDownload?: boolean);
+    setGameType(gameType: GameType): void;
+    setDefinitionDirectory(defDir: string): void;
+    reload(): Promise<void>;
+    loadReplay(replayFile: string, attrib?: NYDUS_ATTRIBUTES): Promise<Replay>;
+    loadProtocol(version: number): Promise<NYDUS_PROTOCOL | null>;
+}
+declare const _default: {
+    Parser: typeof Parser;
+};
+export default _default;
